554695d4425a6bee090c5b54e7f53d9d650dc7a8
Jimi Ford <JimiHFord@users.noreply.github.com>
2017-09-14 01:30:44 -0400

    disable scroll zoom & fix typo

--- src/components/SickPano.jsx
+++ src/components/SickPano.jsx
@@ -9,7 +9,7 @@ export default () => (
       maxHeight: "600px",
       height: "60vh"
     }}
-    src="http://cdn.pannellum.org/2.3/pannellum.htm#panorama=http%3A//www.jones-drones.com/pano.jpg&amp;title=Residential%20Panoramic%20360&amp;author=Jones%20Drones&amp;autoLoad=true" />
+    src="http://cdn.pannellum.org/2.3/pannellum.htm#panorama=http%3A//www.jones-drones.com/pano.jpg&amp;config=https%3A//raw.githubusercontent.com/JimiHFord/jones-drones/master/public/pano-config.json" />
 );
 
 /*
