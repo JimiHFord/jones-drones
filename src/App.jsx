c360f213b5a03449909a4b050e2cc2d1c40b2645
Jimi Ford <JimiHFord@users.noreply.github.com>
2017-09-15 00:22:47 -0400

    fix build

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import { Router, Route, Redirect, Switch } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import ReactGA from 'react-ga';
 import createHistory from "history/createBrowserHistory";
 import Shell from './Shell';
 import Home from './components/Home';
 import MeetTheTeam from './components/meet-the-team';
-// import './App.css';
 
 if (process.env.NODE_ENV === 'production') {
   ReactGA.initialize('UA-99351059-1');
@@ -16,7 +15,8 @@ const recordPageView = location => {
   ReactGA.set({ page: location.pathname });
   ReactGA.pageview(location.pathname);
 };
-const removePageChangeListener = history.listen(recordPageView);
+// const removePageChangeListener = history.listen(recordPageView);
+history.listen(recordPageView);
 recordPageView(window.location);
 
 class App extends Component {
